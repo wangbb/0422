4e4caf1eb20b2794f274744b2d88f264cf699cce
L20102-02 <>
2019-04-22 10:58:33 +0800

    commit 1

---
+++ BB_test_0422/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BB_test_0422
+//
+//  Created by L20102-02 on 2019/4/22.
+//  Copyright © 2019年 L20102-02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BB_test_0422Tests/BB_test_0422Tests.swift
@@ -0,0 +1,34 @@
+//
+//  BB_test_0422Tests.swift
+//  BB_test_0422Tests
+//
+//  Created by L20102-02 on 2019/4/22.
+//  Copyright © 2019年 L20102-02. All rights reserved.
+//
+
+import XCTest
+@testable import BB_test_0422
+
+class BB_test_0422Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
