a8d94f321a3e428357595f5f9a788a73e112ff44
L20102-02 <>
2019-04-22 12:02:43 +0800

    done

--- BB_test_0422/ViewController.swift
+++ BB_test_0422/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MyFirstLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onClicked(_ sender: UIButton) {
+        MyFirstLable.text = "Hello world"
+    }
+    
+    
 }
+
 
